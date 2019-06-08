3173e3289fed5b56ea639a8db695c9320dd10477
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-06-08 15:12:10 +0300

    Saves states in the localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -9,15 +9,21 @@ export const mutations = {
   }
 };
 
+export const STORAGE_KEY = "resume-editor-vuejs-example";
+
+export const plugins = [
+  store => {
+    store.subscribe((mutation, { resume }) => {
+      window.localStorage.setItem(STORAGE_KEY, JSON.stringify(resume));
+    });
+  }
+];
+
 export default new Vuex.Store({
   state: {
-    resume: {
-      firstName: "",
-      lastName: "",
-      subtitle: "",
-      email: ""
-    }
+    resume: JSON.parse(window.localStorage.getItem(STORAGE_KEY) || "{}")
   },
+  plugins,
   mutations,
   actions: {}
 });
