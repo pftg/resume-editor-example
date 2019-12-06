379a80501c0110f826c9232244a140301210a0e1
Andriy <and@Office.local>
2019-12-06 13:54:39 +0200

    Create Quasar app

--- src/store/index.js
+++ src/store/index.js
@@ -1,155 +1,25 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-import api from "@/api/hireclub-client";
+// import example from './module-example'
 
-import * as UserModule from "./user";
+Vue.use(Vuex)
 
-import { cloneDeep, debounce } from "lodash";
+/*
+ * If not building with SSR mode, you can
+ * directly export the Store instantiation
+ */
 
-Vue.use(Vuex);
-
-const state = {
-  api,
-  media: { photo: null },
-  loaded: false,
-  resume: {
-    ...{
-      firstName: "",
-      lastName: "",
-      subtitle: "",
-      email: "",
-      jobs: []
-    }
-  }
-};
-
-export const actions = {
-  async startup({ dispatch, commit }) {
-    commit("loaded", false);
-
-    await dispatch("user/fetch");
-
-    let resume = await api.getResume();
-
-    commit("setResume", resume);
-    commit("loaded", true);
-  },
-
-  updateResume({ commit }, resume) {
-    commit("updateResume", resume);
-  },
-
-  editJob({ commit }, attrs) {
-    commit("editJob", attrs);
-  },
-
-  addJob({ commit }) {
-    commit("addJob", {
-      title: "",
-      company: "",
-      startDate: "",
-      endDate: "",
-      highlights: []
-    });
-  },
-
-  removeJob({ commit }, job) {
-    commit("removeJob", job);
-  },
-
-  editHighlight({ commit }, { highlight, value }) {
-    commit("editHighlight", { highlight, text: value });
-  },
-
-  addHighlight({ commit }, { job, ...highlight }) {
-    commit("addHighlight", { job, highlight });
-  },
-
-  removeHighlight({ commit }, { job, highlight }) {
-    commit("removeHighlight", { job, highlight });
-  }
-};
-
-export const mutations = {
-  loaded(state, status) {
-    state.loaded = status;
-  },
-
-  setResume(state, attrs) {
-    state.resume = { ...state.resume, ...attrs };
-  },
-
-  updateResume(state, attrs) {
-    state.resume = { ...state.resume, ...attrs };
-  },
-
-  addJob(state, job) {
-    state.resume.jobs.push(job);
-  },
-  // TODO: We need to find best way to pass such stuff. There is 2 options like this or like above
-  editJob(state, { job, ...attrs }) {
-    // state.resume.job = { ...job, ...attrs };
-    // instead of assigment new value to job, we change only some attrs
-    // we have everything predefined, this is not new attrs
-    Object.assign(job, attrs);
-  },
-
-  removeJob(state, job) {
-    state.resume.jobs.splice(state.resume.jobs.indexOf(job), 1);
-  },
-
-  editHighlight(state, { highlight, text = highlight.text }) {
-    highlight.text = text;
-  },
-
-  addHighlight(state, { job, highlight }) {
-    job.highlights.push(highlight);
-  },
-
-  removeHighlight(state, { job, highlight }) {
-    job.highlights.splice(job.highlights.indexOf(highlight), 1);
-  }
-};
-
-export const autoSaverPlugin = store => {
-  let prevState = cloneDeep(store.state.resume);
-
-  const updateOnApi = debounce(
-    (resume, fail) => {
-      api
-        .updateResume(resume)
-        .then(() => console.log("success in resume update"))
-        .catch(errorMessage => {
-          console.error(errorMessage);
-          fail();
-        });
+export default function (/* { ssrContext } */) {
+  const Store = new Vuex.Store({
+    modules: {
+      // example
     },
-    1500,
-    { leading: true, maxWait: 3000 }
-  );
 
-  store.subscribe((mutation, { resume }) => {
-    if (mutation.type === "updateResume" || mutation.type === "editJob") {
-      const prevResume = prevState;
-      updateOnApi(resume, () => {
-        console.error("Reverting resume to previous version");
-        store.commit("setResume", prevResume);
-      });
-      prevState = cloneDeep(resume);
-    }
-  });
-};
+    // enable strict mode (adds overhead!)
+    // for dev mode only
+    strict: process.env.DEV
+  })
 
-export const plugins = [autoSaverPlugin];
-
-export default new Vuex.Store({
-  modules: {
-    user: UserModule
-  },
-
-  state,
-  plugins,
-  mutations,
-  actions
-});
+  return Store
+}
