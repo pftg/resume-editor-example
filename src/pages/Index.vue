f69c2bfaff49e165746464d7077b122956aae90e
Andriy <and@Office.local>
2019-12-10 19:54:51 +0200

    Show avatar in preview

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,109 +4,115 @@
 
       <div class="q-pa-md q-mb-lg shadow-2 rounded-borders">
         <q-scroll-area class="scroll-area">
-        <div id="photo">
-          <div class="text-weight-bold">Photo</div>
-          <span>Upload your photo</span>
-          <q-separator class="q-mt-md q-mb-md" />
-          <q-uploader
-            ref="uploader"
-          />
-        </div>
+          <div id="photo">
+            <div class="text-weight-bold">Photo</div>
+            <span>Upload your photo</span>
+            <q-separator class="q-mt-md q-mb-md" />
+            <input type="file" @change="onFileSelected" />
+          </div>
+          <div id="name">
+            <div class="text-weight-bold">Name</div>
+            <span>Use your first and last name. No nicknames</span>
+            <q-separator class="q-mt-md q-mb-md" />
+            <q-form class="q-gutter-md overflow-hidden">
+              <q-input
+                filled
+                v-model="firstName"
+                label="First Name"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+              <q-input
+                filled
+                v-model="lastName"
+                label="Last Name"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+            </q-form>
+          </div>
 
-        <div id="name">
-          <div class="text-weight-bold">Name</div>
-          <span>Use your first and last name. No nicknames</span>
-          <q-separator class="q-mt-md q-mb-md" />
-          <q-form class="q-gutter-md overflow-hidden">
-            <q-input
-              filled
-              v-model="firstName"
-              label="First Name"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-            <q-input
-              filled
-              v-model="lastName"
-              label="Last Name"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-          </q-form>
-        </div>
+          <div id="subtitle">
+            <div class="text-weight-bold">Subtitle</div>
+            <span>Two words what you want to do</span>
+            <q-separator class="q-mt-md q-mb-md" />
+            <q-form class="q-gutter-md overflow-hidden">
+              <q-input
+                filled
+                v-model="subtitle"
+                label="Subtitle"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+            </q-form>
+          </div>
 
-        <div id="subtitle">
-          <div class="text-weight-bold">Subtitle</div>
-          <span>Two words what you want to do</span>
-          <q-separator class="q-mt-md q-mb-md" />
-          <q-form class="q-gutter-md overflow-hidden">
-            <q-input
-              filled
-              v-model="subtitle"
-              label="Subtitle"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-          </q-form>
-        </div>
+          <div id="email">
+            <div class="text-weight-bold">Email</div>
+            <span>professional email</span>
+            <q-separator class="q-mt-md q-mb-md" />
+            <q-form class="q-gutter-md overflow-hidden">
+              <q-input
+                filled
+                v-model="email"
+                label="paul.keen@jetthoughts.com"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+            </q-form>
+          </div>
 
-        <div id="email">
-          <div class="text-weight-bold">Email</div>
-          <span>professional email</span>
-          <q-separator class="q-mt-md q-mb-md" />
-          <q-form class="q-gutter-md overflow-hidden">
-            <q-input
-              filled
-              v-model="email"
-              label="paul.keen@jetthoughts.com"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-          </q-form>
-        </div>
-
-        <div id="experience">
-          <div class="text-weight-bold">Experience</div>
-          <span>Add at least 3 jobs with 3 highlights each</span>
-          <q-separator class="q-mt-md q-mb-md" />
-          <q-form class="q-gutter-md overflow-hidden">
-            <q-input
-              filled
-              v-model="title"
-              label="Title"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-            <q-input
-              filled
-              v-model="company"
-              label="Company"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-            <q-input
-              filled
-              v-model="highlights"
-              label="New Highlights"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-          </q-form>
-        </div>
-      </q-scroll-area>
+          <div id="experience">
+            <div class="text-weight-bold">Experience</div>
+            <span>Add at least 3 jobs with 3 highlights each</span>
+            <q-separator class="q-mt-md q-mb-md" />
+            <q-form class="q-gutter-md overflow-hidden">
+              <q-input
+                filled
+                v-model="title"
+                label="Title"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+              <q-input
+                filled
+                v-model="company"
+                label="Company"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+              <q-input
+                filled
+                v-model="highlights"
+                label="New Highlights"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+            </q-form>
+          </div>
+        </q-scroll-area>
       </div>
 
     </div>
     <div class="col-5 q-pa-md">
       <div>
-        <h4 class="q-my-sm text-h4">
-          <span class="q-mr-sm">{{ firstName }}</span>
-          <span>{{ lastName }}</span>
-        </h4>
-        <div class="text-subtitle1">{{ subtitle }}</div>
-        <div>
-          <span class="text-weight-bold text-subtitle1 q-mr-sm">Email:</span>
-          <span class="text-body2">{{ email }}</span>
+        <img
+          class="float-left q-mr-md"
+          width="200"
+          v-if="img"
+          :src="img"
+          alt="Avatar"
+        />
+        <div class="overflow-hidden">
+          <h4 class="q-my-sm text-h4">
+            <span class="q-mr-sm">{{ firstName }}</span>
+            <span>{{ lastName }}</span>
+          </h4>
+          <div class="text-subtitle1">{{ subtitle }}</div>
+          <div>
+            <span class="text-weight-bold text-subtitle1 q-mr-sm">Email:</span>
+            <span class="text-body2">{{ email }}</span>
+          </div>
         </div>
       </div>
 
@@ -114,7 +120,7 @@
         <q-separator class="q-mt-md q-mb-md" />
         <h6 class="text-h6 q-mb-sm q-mt-none">Education:</h6>
         <span class="text-subtitle1">{{ title }}</span>
-        <span class="text-subtitle1 q-mx-sm">at</span>
+        <span class="text-subtitle1 q-mx-sm" v-if="company">at</span>
         <span class="text-subtitle1">{{ company }}</span>
         <div class="text-body2">{{ highlights }}</div>
       </div>
@@ -123,7 +129,7 @@
         <q-separator class="q-mt-md q-mb-md" />
         <h6 class="text-h6 q-mb-sm q-mt-none">Experience:</h6>
         <span class="text-subtitle1">{{ title }}</span>
-        <span class="text-subtitle1 q-mx-sm">at</span>
+        <span class="text-subtitle1 q-mx-sm" v-if="company">at</span>
         <span class="text-subtitle1">{{ company }}</span>
         <div class="text-body2">{{ highlights }}</div>
       </div>
@@ -133,8 +139,15 @@
 
 <script>
 export default {
+  methods: {
+    onFileSelected (event) {
+      this.$data.img = URL.createObjectURL(event.target.files[0])
+    }
+  },
+
   data () {
     return {
+      img: 'blob:http://localhost:8080/dfecf2a8-a686-495e-8236-4c89d2398668',
       panel: 'name',
       firstName: 'John',
       lastName: 'Wayne',
